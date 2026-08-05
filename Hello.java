c2aa4a98243304d3d4a5b55408f8d81e16684091
Praveen G <guduripraveenkumar@gmail.com>
2026-08-06 04:59:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello{
 public static void main(String[] args){
   String name = System.getenv("USERNAME_VAR");
   if(name != null){
-  System.out.println("WELCOME TO DEVOPS USER" + " " + name);
+  System.out.println("WELCOME TO DEVOPS ENGINEER" + " " + name);
 } else{
   System.out.println("Environment variable not set1");
 }
